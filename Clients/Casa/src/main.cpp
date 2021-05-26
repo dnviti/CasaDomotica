51ac31ee4b7405409863676dab8a70467df47dc0
Daniele Viti <dnviti@gmail.com>
2021-05-27 01:23:14 +0200

    Keypad I2C

--- Clients/Casa/src/main.cpp
+++ Clients/Casa/src/main.cpp
@@ -1,41 +1,37 @@
 /*
- Name:		ArduinoCasaDomotica2.ino
+ Name:		ArduinoCasaDomotica2
  Created:	5/26/2021 00:47:50 AM
  Author:	Daniele Viti
 */
 
-/* #region Includes */
 #include <Arduino.h>
 #include <PubSubClient.h>
 #include <ESP8266WiFi.h>
 #include <Wire.h>
 #include <SPI.h>
+#include <Key.h>
 #include <Keypad.h>
+#include <Keypad_I2C.h>
 #include <Countimer.h>
 #include <U8g2lib.h>
-/* #endregion */
 
-/* #region Defines */
-#define LED_INT1_PIN D1
-#define LED_EXT2_PIN D2
-#define LED_INT3_PIN D3
-#define LED_EXT4_PIN D4
+#define LED_INT1_PIN D5
+#define LED_INT3_PIN D7
+#define LED_EXT2_PIN D0
+#define LED_EXT4_PIN D6
+#define PHOTORESISTOR_PIN A0
 
 #define DEBUGLN(A) Serial.println(A);
 #define DEBUG(A) Serial.print(A);
 #define DEBUG_ENABLED 1
 
 #define MSG_BUFFER_SIZE (50)
-/* #endregion */
 
-/* #region Variables */
+#define I2CADDR 0x20 // Set the Address of the PCF8574
 
-/* #region Timers */
 Countimer tDisplay;
 Countimer tReadings;
-/* #endregion */
 
-/* #region OLED Display */
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE, /* clock=*/SCL, /* data=*/SDA); // pin remapping with ESP8266 HW I2C
 
 String displayTopLeft = "";
@@ -44,9 +40,7 @@ String displayBottomLeft = "";
 String displayBottomRight = "";
 
 int displayState = 0;
-/* #endregion */
 
-/* #region Mqtt Variables */
 WiFiClient espClient;
 PubSubClient client(espClient);
 
@@ -68,11 +62,18 @@ const char *mqttInOutTimeOfDay = "ard2/home1/timeofday";
 // Output
 const char *mqttOutEarthquake = "ard2/home1/earthquake";
 
-/* #endregion */
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'},
+    {'*', '0', '#'}};
+byte rowPins[ROWS] = {0, 1, 2, 3}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {4, 5, 6};    //connect to the column pinouts of the keypad
+Keypad_I2C keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS, I2CADDR, PCF8574);
 
-/* #endregion */
-
-/* #region Utility */
+/* #region Helpers */
 void debugLog(String s = "", bool newLine = true)
 {
   if (DEBUG_ENABLED == 1)
@@ -175,7 +176,6 @@ void reconnect()
   }
 }
 
-/* #region Generic Send Mqtt Value */
 void sendMqttValue(const char *mqttTopic, char *val)
 {
   //debugLog(String(val));
@@ -202,8 +202,6 @@ void sendMqttValue(const char *mqttTopic, String val)
   val.toCharArray(msg, 50);
   sendMqttValue(mqttTopic, msg);
 }
-/* #endregion */
-
 /* #endregion */
 
 /* #region Business Logic */
@@ -246,8 +244,10 @@ void callback(char *topic, byte *payload, unsigned int length)
 }
 /* #endregion */
 
-/* #region Timers Definitions */
-void tDisplayComplete() {}
+/* #region Timers */
+void tDisplayComplete()
+{
+}
 void tDisplayInterval()
 {
   char displayTopLeftBuf[50];
@@ -280,11 +280,15 @@ void tReadingsInterval()
 
 void setup()
 {
-  /* #region Setup Display */
-  u8g2.begin();
-  /* #endregion */
+  Serial.begin(115200);
+  debugLog("Starting Pin Extender");
+  Wire.begin();                   // Call the connection Wire
+  keypad.begin(makeKeymap(keys)); // Call the connection
 
-  /* #region Setup Timers */
+  debugLog("Starting Display");
+  u8g2.begin();
+
+  debugLog("Starting Timers");
   tDisplay.setCounter(24, 0, 0, tDisplay.COUNT_DOWN, tDisplayComplete);
   tDisplay.setInterval(tDisplayInterval, 1);
   tDisplay.start();
@@ -292,16 +296,20 @@ void setup()
   tReadings.setCounter(24, 0, 0, tReadings.COUNT_DOWN, tReadingsComplete);
   tReadings.setInterval(tReadingsInterval, 1000);
   tReadings.start();
-  /* #endregion */
 
-  /* #region Setup Pins */
+  debugLog("Setup Pinout");
   pinMode(LED_INT1_PIN, OUTPUT);
   pinMode(LED_EXT2_PIN, OUTPUT);
   pinMode(LED_INT3_PIN, OUTPUT);
   pinMode(LED_EXT4_PIN, OUTPUT);
-  /* #endregion */
+  pinMode(PHOTORESISTOR_PIN, INPUT);
 
-  /* #region Setup MQTT */
+  digitalCommonAnodeWrite(LED_INT1_PIN, LOW);
+  digitalCommonAnodeWrite(LED_INT3_PIN, LOW);
+  digitalCommonAnodeWrite(LED_EXT2_PIN, LOW);
+  digitalCommonAnodeWrite(LED_EXT4_PIN, LOW);
+
+  debugLog("Setup MQTT");
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
@@ -314,38 +322,35 @@ void setup()
     client.subscribe(mqttInOutLed4);
     client.subscribe(mqttInOutTimeOfDay);
   }
-  /* #endregion */
 
-  /* #region Value Presets */
   // Setto il valore del pin del led per condividerlo con tutti all'avvio
+  /*
   int led1Val = digitalCommonAnodeRead(LED_INT1_PIN);
   debugLog("Initial Led Value: " + String(led1Val));
   snprintf(msg, MSG_BUFFER_SIZE, "%d", led1Val);
   client.publish(mqttInOutLed1, msg);
-
-  int led2Val = digitalCommonAnodeRead(LED_EXT2_PIN);
-  debugLog("Initial Led Value: " + String(led2Val));
-  snprintf(msg, MSG_BUFFER_SIZE, "%d", led2Val);
-  client.publish(mqttInOutLed2, msg);
-
-  int led3Val = digitalCommonAnodeRead(LED_INT3_PIN);
-  debugLog("Initial Led Value: " + String(led3Val));
-  snprintf(msg, MSG_BUFFER_SIZE, "%d", led3Val);
-  client.publish(mqttInOutLed3, msg);
-
-  int led4Val = digitalCommonAnodeRead(LED_EXT4_PIN);
-  debugLog("Initial Led Value: " + String(led4Val));
-  snprintf(msg, MSG_BUFFER_SIZE, "%d", led4Val);
-  client.publish(mqttInOutLed4, msg);
-  /* #endregion */
-
-  Serial.begin(115200);
+  */
 }
 
 void loop()
 {
-  digitalCommonAnodeWrite(LED_INT1_PIN, HIGH);
-  digitalCommonAnodeWrite(LED_EXT2_PIN, HIGH);
-  digitalCommonAnodeWrite(LED_INT3_PIN, HIGH);
-  digitalCommonAnodeWrite(LED_EXT4_PIN, HIGH);
+  char key = keypad.getKey();
+
+  if (key)
+  {
+    debugLog(String(key));
+  }
+
+  int phrValue = analogRead(PHOTORESISTOR_PIN);
+
+  if (phrValue <= 50)
+  {
+    digitalCommonAnodeWrite(LED_EXT2_PIN, HIGH);
+    digitalCommonAnodeWrite(LED_EXT4_PIN, HIGH);
+  }
+  else
+  {
+    digitalCommonAnodeWrite(LED_EXT2_PIN, LOW);
+    digitalCommonAnodeWrite(LED_EXT4_PIN, LOW);
+  }
 }
